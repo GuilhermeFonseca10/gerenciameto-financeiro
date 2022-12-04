513008bbaffb1423bde3b6c89a4c456b1e9f4b2e
GuilhermeFonseca10 <guilhermeelviro2017@gmail.com>
2022-12-03 22:00:28 -0300

    gerenciamento financeiro

---
+++ conta/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from conta.models import Conta
+
+admin.site.register(Conta)
+
+# Register your models here.
---
+++ conta/models.py
@@ -0,0 +1,24 @@
+#coding: utf-8
+from django.db import models
+from django.core.urlresolvers import reverse
+from usuario.models import Usuario
+
+
+class Conta(models.Model):
+    descricao = models.CharField(u'Descrição', max_length=40)
+    saldo = models.DecimalField(u'Saldo', max_digits=10, decimal_places=2, default=0)
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.descricao)
+
+    def __unicode__(self):
+        return '%s: R$ %d' % (self.descricao, self.saldo)
+
+    class Meta:
+        ordering = ['-saldo','descricao']
+
+
+    @property
+    def get_absolute_url(self):
+        return reverse('conta_update', args=[str(self.id)])
