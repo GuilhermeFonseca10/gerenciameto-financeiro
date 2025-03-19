013366de21759044eefd5a080c17029053d3f592
Guilherme Fonseca <guilhermeelviro2017@gmail.com>
2025-03-19 10:57:36 -0300

    feat: Adiciona gastos e lucros por categorias

--- categoria/urls.py
+++ categoria/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from .views import CategoriaCreateView, CategoriaListView, CategoriaUpdateView, CategoriaDeleteView
+from .views import (
+    CategoriaCreateView, 
+    CategoriaListView, 
+    CategoriaUpdateView, 
+    CategoriaDeleteView, 
+    DespesasPorCategoriaView, 
+    LucrosPorCategoriaView
+)
 
 urlpatterns = [
     path('list/', CategoriaListView.as_view(), name='categoria_list'),
     path('cad/', CategoriaCreateView.as_view(), name='categoria_create'),
     path('<int:pk>/', CategoriaUpdateView.as_view(), name='categoria_update'),
     path('<int:pk>/delete/', CategoriaDeleteView.as_view(), name='categoria_delete'),
+    path("<int:pk>/despesas/", DespesasPorCategoriaView.as_view(), name="despesas_por_categoria"),
+    path("<int:pk>/lucros/", LucrosPorCategoriaView.as_view(), name="lucros_por_categoria"),
+    
 ]
--- categoria/views.py
+++ categoria/views.py
@@ -5,6 +5,8 @@ from django.urls import reverse_lazy
 from utils.decorators import LoginRequiredMixin
 
 from .models import Categoria
+from lancamento.models import Lancamento
+from lucro.models import Lucro
 
 
 class CategoriaListView(LoginRequiredMixin, ListView):
@@ -26,3 +28,30 @@ class CategoriaUpdateView(LoginRequiredMixin, UpdateView):
 class CategoriaDeleteView(LoginRequiredMixin, DeleteView):
     model = Categoria
     success_url = reverse_lazy("categoria_list")
+    
+class DespesasPorCategoriaView(LoginRequiredMixin, ListView):
+    template_name = "categoria/despesas_por_categoria.html"
+    context_object_name = "despesas"
+
+    def get_queryset(self):
+        categoria_id = self.kwargs["pk"]
+        return Lancamento.objects.filter(categorias__id=categoria_id, usuario=self.request.user)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categoria_id = self.kwargs["pk"]
+        context['categoria'] = Categoria.objects.get(id=categoria_id)  # Obtém a categoria com o id
+        return context
+
+class LucrosPorCategoriaView(LoginRequiredMixin, ListView):
+    template_name = "categoria/lucros_por_categoria.html"
+    context_object_name = "lucros"
+
+    def get_queryset(self):
+        categoria_id = self.kwargs["pk"]
+        return Lucro.objects.filter(categorias__id=categoria_id, usuario=self.request.user)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categoria_id = self.kwargs["pk"]
+        context['categoria'] = Categoria.objects.get(id=categoria_id)  # Obtém a categoria com o id
+        return context
+
