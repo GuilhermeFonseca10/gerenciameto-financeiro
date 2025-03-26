623ee31ea1ba2cab502ce3b6c15900604cfc9d37
Guilherme Fonseca <guilhermeelviro2017@gmail.com>
2025-03-26 10:22:57 -0300

    refactor: Melhora o estilo da pagina home

--- projeto/settings.py
+++ projeto/settings.py
@@ -137,4 +137,4 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "core/static/"
