5e3aee3916f7570b70d23eba07629065c60fb033
Guilherme Fonseca <guilhermeelviro2017@gmail.com>
2025-05-12 13:41:29 -0300

    feat: Resolve erro para deploy

--- projeto/settings.py
+++ projeto/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'core.middleware.CustomPermissionDeniedMiddleware',
     'crum.CurrentRequestUserMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "projeto.urls"
